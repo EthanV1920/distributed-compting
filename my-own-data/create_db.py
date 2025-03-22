087639ad2d8e43f1e2fdd0c1c1ab2114f4b4ad48
Ethan Vosburg <63841608+EthanV1920@users.noreply.github.com>
2025-03-22 00:54:32 -0700

    Initial analysis of the streaming data and construction of the database

---
+++ my-own-data/create_db.py
@@ -0,0 +1,32 @@
+"""
+Filename: create_db.py
+Author: Ethan Vosburg
+Date: March 20, 2025
+Version: 1.0
+Description: This python file makes and creates a database from the csv file
+             in duckdb.
+"""
+
+# Imports
+import duckdb
+
+con = duckdb.connect("stream.db")
+
+# Create the table from the csv
+creation_sql = """
+    CREATE TABLE twitch_data AS
+    FROM '../my-own-data/full_a.csv';
+"""
+
+# con.sql(creation_sql)
+
+# Rename columns
+rename_sql = """
+ALTER TABLE twitch_data RENAME column0 TO stream_id;
+ALTER TABLE twitch_data RENAME column1 TO user_id;
+ALTER TABLE twitch_data RENAME column2 TO streamer_id;
+ALTER TABLE twitch_data RENAME column3 TO start_time;
+ALTER TABLE twitch_data RENAME column4 TO end_time;
+"""
+
+con.sql(rename_sql)
